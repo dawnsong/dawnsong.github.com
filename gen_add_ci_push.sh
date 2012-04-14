f59470a874582acb211ef1efc5d63e02402440d4
dawnsong <dawnsong.tsinghua@gmail.com>
2012-04-14 19:12:43 +0800

    1. add mathjax support in source/_includes/head.html

---
+++ gen_add_ci_push.sh
@@ -0,0 +1,35 @@
+#!/bin/bash - 
+#===============================================================================
+#
+#          FILE:  gen_add_ci_push.sh
+# 
+#         USAGE:  ./gen_add_ci_push.sh 
+# 
+#   DESCRIPTION:  
+# 
+#       OPTIONS:  ---
+#  REQUIREMENTS:  ---
+#          BUGS:  ---
+#         NOTES:  ---
+#        AUTHOR: dawnsong (Xiao-Wei Song), dawnsong@moon.ibp.ac.cn
+#       COMPANY: Institute of Biophysics, Chinese Academy of Science
+#       CREATED: 04/14/2012 07:11:12 PM CST
+#      REVISION:  ---
+#===============================================================================
+
+set -o nounset                              # Treat unset variables as an error
+
+elog(){ echo "$@" 1>&2 ; }
+
+#if [ $# -eq 0 ]; then
+#  elog "Usage: $0 " 
+#  exit 1
+#fi 
+
+cd $(dirname `readlink -f $0`)
+
+rake generate
+git add .
+git commit
+git push heroku master
+git push github
